c96018a3b0fd14d95ac1722458632b19e163e404
DeHkO1988 <denislav.bogomilov88@gmail.com>
2023-11-04 07:13:34 +0200

    add css and html

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,193 @@
+import { useState } from 'react'
+
 
 function App() {
 
-
     return (
         <>
+            <div className="header">
+                <div className="header-left-panel">
+                    <div className="logo-wrap">
+                        <div className="logo">
+                            <h2>your slogan</h2>
+                            <h1>WebsiteName</h1>
+                        </div>
+                    </div>
+                </div>
+                <div className="header-right-panel">
+                    <div className="header-right-panel-top">
+                        <p>Call Us : 000 888 8888</p>
+                        <p> <a href="#">Mail Us : info@websitename.com</a></p>
+                    </div>
+                    <div className="menu">
+                        <ul>
+                            <li className="marRight20"><a className="active" href="#">home</a></li>
+                            <li className="marRight20"><a href="#">about</a></li>
+                            <li><a href="#">services</a></li>
+                            <li className="marRight20"><a href="#">features</a></li>
+                            <li className="marRight20"><a href="#">blog</a></li>
+                            <li><a href="contact.html">contact</a></li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+
+            <div className="panel-wrap">
+                <div class="top-content">
+                    <h1>Aliquam arcu arcu aliquam eu</h1>
+                </div>
+                <div className="panel-wrapper">
+                    <div className="panel">
+                        <div className="img"><img className='image' src="https://media.architecturaldigest.com/photos/63079fc7b4858efb76814bd2/16:9/w_4000,h_2250,c_limit/9.%20DeLorean-Alpha-5%20%5BDeLorean%5D.jpg" /></div>
+                        <div className="title">
+                            <h1>Lorem ipsum</h1>
+                        </div>
+                        <div className="border"></div>
+                        <div className="content">
+                            <p>Proin tempus leo id quam pellentesque sollic
+                                itudin adipiscing velit imperdiet.</p>
+                            <div className="button-link"><a href="#">details</a></div>
+                        </div>
+                    </div>
+                    <div className="panel">
+                        <div className="img"><img className='image' src="https://media.architecturaldigest.com/photos/63079fc7b4858efb76814bd2/16:9/w_4000,h_2250,c_limit/9.%20DeLorean-Alpha-5%20%5BDeLorean%5D.jpg" /></div>
+                        <div className="title">
+                            <h1>Lorem ipsum</h1>
+                        </div>
+                        <div className="border"></div>
+                        <div className="content">
+                            <p>Proin tempus leo id quam pellentesque sollic
+                                itudin adipiscing velit imperdiet.</p>
+                            <div className="button-link"><a href="#">readmore</a></div>
+                        </div>
+                    </div>
+                    <div className="panel">
+                        <div className="img"><img className='image' src="https://media.architecturaldigest.com/photos/63079fc7b4858efb76814bd2/16:9/w_4000,h_2250,c_limit/9.%20DeLorean-Alpha-5%20%5BDeLorean%5D.jpg" /></div>
+                        <div className="title">
+                            <h1>Lorem ipsum</h1>
+                        </div>
+                        <div className="border"></div>
+                        <div className="content">
+                            <p>Proin tempus leo id quam pellentesque sollic
+                                itudin adipiscing velit imperdiet.</p>
+                            <div className="button-link"><a href="#">readmore</a></div>
+                        </div>
+                    </div>
+                    <div className="panel">
+                        <div className="img"><img className='image' src="https://media.architecturaldigest.com/photos/63079fc7b4858efb76814bd2/16:9/w_4000,h_2250,c_limit/9.%20DeLorean-Alpha-5%20%5BDeLorean%5D.jpg" /></div>
+                        <div className="title">
+                            <h1>Lorem ipsum</h1>
+                        </div>
+                        <div className="border"></div>
+                        <div className="content">
+                            <p>Proin tempus leo id quam pellentesque sollic
+                                itudin adipiscing velit imperdiet.</p>
+                            <div className="button-link"><a href="#">readmore</a></div>
+                        </div>
+                    </div>
+                    <div className="panel">
+                        <div className="img"><img className='image' src="https://media.architecturaldigest.com/photos/63079fc7b4858efb76814bd2/16:9/w_4000,h_2250,c_limit/9.%20DeLorean-Alpha-5%20%5BDeLorean%5D.jpg" /></div>
+                        <div className="title">
+                            <h1>Lorem ipsum</h1>
+                        </div>
+                        <div className="border"></div>
+                        <div className="content">
+                            <p>Proin tempus leo id quam pellentesque sollic
+                                itudin adipiscing velit imperdiet.</p>
+                            <div className="button-link"><a href="#">readmore</a></div>
+                        </div>
+                    </div>
+
+
+                </div>
+            </div>
+
+
+
+
+
+            <div class="page-wrap">
+
+                <div class="top-border"></div>
+                <form class="search-panel">
+                    <div class="content">
+                        <div class="title">
+                            <h1 className='border'>Lorem ipsum dolo</h1>
+                        </div>
+
+                        <div class="searchbox">
+                            <label class="label">Search the site here...</label><br />
+                            <input type="text" class="input" name='1' /><br />
+                            <label>Search the site here...</label><br />
+                            <input type="text" class="input" name='2' /><br />
+                            <label>Search the site here...</label><br />
+                            <input type="text" class="input" name='3' /><br />
+                            <label class="label">Search the site here...</label><br />
+                            <textarea class="input text" name='4'></textarea>
+
+                        </div>
+
+                        <button class="button" type="submit">Submit</button>
+                    </div>
+                </form>
+
+
+                <div class="page-wrapper">
+                    <div class="primary-content">
+
+                        <div class="mid-panel marginTop">
+                            <div class="mid-panel-content">
+                                <div class="title">
+                                    <h1>Aliquam arcu arcu aliquam eu</h1>
+                                </div>
+                                <div class="border"></div>
+                                <h2>Subheading goes here</h2>
+                                <p>Phasellus non arcu id nisi ullamcorper varius. In quis vehicula enim. Maecenas gravida metus
+                                    vitae tortor
+                                    cursus sed mattis velit congue. Cras id nisi massa, sed feugiat est. </p>
+                                <img class=" image" src="https://imageio.forbes.com/specials-images/imageserve/5d35eacaf1176b0008974b54/2020-Chevrolet-Corvette-Stingray/0x0.jpg?format=jpg&crop=4560,2565,x790,y784,safe&width=960" />
+                                <img class=" image" src="https://imageio.forbes.com/specials-images/imageserve/5d35eacaf1176b0008974b54/2020-Chevrolet-Corvette-Stingray/0x0.jpg?format=jpg&crop=4560,2565,x790,y784,safe&width=960" />
+                                <img class=" image" src="https://imageio.forbes.com/specials-images/imageserve/5d35eacaf1176b0008974b54/2020-Chevrolet-Corvette-Stingray/0x0.jpg?format=jpg&crop=4560,2565,x790,y784,safe&width=960" />
+
+                            </div>
+                        </div>
+
+
+                    </div>
+
+                </div>
+                <div class="page-wrapper">
+
+                    <div class="primary-content">
+                        <div class="mid-panel marginTop">
+                            <div class="mid-panel-content ">
+                                <div class="title">
+                                    <h1>Aliquam arcu arcu aliquam eu</h1>
+                                </div>
+                                <div class="border"></div>
+                                <div>
+                                    <img class="catalogImage" src="https://www.autocar.co.uk/sites/autocar.co.uk/files/images/car-reviews/first-drives/legacy/10-porsche-718-cayman-gt4-rs-top-10.jpg" />
+                                    <h2>Title</h2>
+                                    <p><strong>Price:</strong></p>
+                                    <p><strong>Price:</strong></p>
+                                    <p><strong>Price:</strong></p>
+                                    <p><strong>Price:</strong></p>
+                                    <p><strong>Price:</strong></p>
+
+                                    <p class="padBottom">Cras venenatis lacus vitae nisl tincidunt varius. Nunc dictum dapibus neque</p>
+                                </div>
+                                <p class="padBottom">Cras venenatis lacus vitae nisl tincidunt varius. Nunc dictum dapibus neque</p>
+
+                            </div>
+                        </div>
+
+                    </div>
+
+                </div>
+            </div>
+            <div class="footer">
+                <p>Copyright (c) websitename. All rights reserved</p>
+            </div>
 
         </>
     )
