ad7f182d308f58bece935b64267d19deb9f0b88a
DeHkO1988 <denislav.bogomilov88@gmail.com>
2023-12-08 21:22:01 +0200

    validation function refakturing

--- client/src/components/Create/validation.jsx
+++ client/src/components/Create/validation.jsx
@@ -7,39 +7,27 @@ export const validation = (data) => {
 
     if (data.brand.length < 5) {
         result.brand = 'At least 5 chars.';
-    } else {
-        delete result.brand;
     };
 
     if (data.fuel.length < 5) {
         result.fuel = 'At least 5 chars.';
-    } else {
-        delete result.fuel;
     };
 
     if (data.mileage <= 0) {
         result.mileage = 'Have to be positive number.';
-    } else {
-        delete result.mileage;
     };
 
     if (data.registration < 1900 || data.registration > 2023) {
         result.registration = 'Number between 1900 and 2023.';
-    } else {
-        delete result.registration;
     };
 
     if (!imgPattern.test(data.imageUrl)) {
         result.imageUrl = 'Picture URL must start whit http or https.';
-    } else {
-        delete result.imageUrl;
-    }
+    };
 
     if (data.description.length < 10) {
         result.description = 'At least 10 chars.';
-    } else {
-        delete result.description;
-    }
+    };
 
     return result;
 
--- client/src/components/Register/errors.jsx
+++ client/src/components/Register/errors.jsx
@@ -8,26 +8,18 @@ export const test = (registerData) => {
 
     if (!emailPattern.test(registerData.email)) {
         result.email = 'Valid e-mail address.';
-    } else {
-        delete result.email
     };
-
+    
     if (!usernamePattern.test(registerData.username) || registerData.username.length < 3) {
         result.username = 'More then 3 chars.';
-    } else {
-        delete result.username;
     };
 
     if (registerData.password.length < 5) {
         result.password = 'More then 4 chars.';
-    } else {
-        delete result.password;
     };
 
     if (registerData.repeatPassword !== registerData.password || registerData.repeatPassword.length < 5) {
         result.repeatPassword = 'Same as password.';
-    } else {
-        delete result.repeatPassword;
     };
 
     return result;
